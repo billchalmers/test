c046240ce6d23673e385b055a2a4061143eed2c8
Bill Chalmers <billchalmers@gmail.com>
2019-04-10 11:40:04 +0100

    te

---
+++ dradis/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Exit the script in case of errors
+set -e
+
+SECRET_KEY_BASE="${SECRET_KEY_BASE:-$(rake secret)}"
+export SECRET_KEY_BASE
+
+cp -n /opt/dradis/db/production.sqlite3 /dbdata/
+chown -R dradis /dbdata/
+chmod -R u+w /dbdata/
+
+if [ -z "${*}" ]
+then
+  exec su -m -l dradis -c 'exec bundle exec rails server'
+else
+  exec su -m -l dradis -c 'exec bundle exec rails server "$0" "$@"' -- "${@}"
+fi
